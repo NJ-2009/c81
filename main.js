a965a82319a1bb19e009d5d703a37b75b2c2b2ca
NJ-2009 <74959872+NJ-2009@users.noreply.github.com>
2022-02-05 18:46:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+canvas=
+  document.getElementById("mycanvas")
+ctx=canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokestyle="red";
+ctx.linewidth=10;
+ctx.rect(150,143,430,200);
+ctx.beginPath();
+ctx.strokestyle="blue";
+ctx.linewidth=10;
+ctx.arc(250,210,40,0,2* Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokestyle="yellow";
+ctx.linewidth=10;
+ctx.arc(285,270,40,0,2* Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokestyle="black";
+ctx.linewidth=10;
+ctx.arc(330,210,40,0,2* Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokestyle="green";
+ctx.linewidth=10;
+ctx.arc(370,270,40,0,2* Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokestyle="red";
+ctx.linewidth=10;
+ctx.arc(400,210,40,0,2* Math.PI);
+ctx.stroke();
